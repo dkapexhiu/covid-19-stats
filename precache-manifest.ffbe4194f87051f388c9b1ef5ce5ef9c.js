c26a5983cb4bb3f89e05407820aad14782e08ede
Carlos Bonadeo <carlosbonadeo@gmail.com>
2020-03-24 15:44:28 -0300

    deploy

---
+++ precache-manifest.ffbe4194f87051f388c9b1ef5ce5ef9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2c0d83dfbbb6d60ade52a777100caf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "618e364b19e3aecfe55c",
+    "url": "./static/css/main.775bd640.chunk.css"
+  },
+  {
+    "revision": "ffab8f392eca36a86646",
+    "url": "./static/js/2.a2ddaf67.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.a2ddaf67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "618e364b19e3aecfe55c",
+    "url": "./static/js/main.e06a380e.chunk.js"
+  },
+  {
+    "revision": "d29a336a6ce5e7c16f8d",
+    "url": "./static/js/runtime-main.dd8e06fa.js"
+  },
+  {
+    "revision": "7215a9bdfac69f0d0a71cdb53d315bf0",
+    "url": "./static/media/github.7215a9bd.svg"
+  }
+]);
